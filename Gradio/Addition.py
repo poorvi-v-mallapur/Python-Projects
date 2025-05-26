c836e84940c5d3db22b8a8f29ece1e5d98ac46d5
poorvi-v-mallapur <purvigowda88@gmail.com>
2025-05-26 18:22:54 +0530

    Addition of numbers on gradio interface

--- Gradio/Addition.py
+++ Gradio/Addition.py
@@ -1,11 +1,11 @@
 import gradio as gr
 
-def add_nums(a, b):
-    return a + b
+def add_nums(a, b, c):
+    return a + b + c
 
 interface = gr.Interface(
     fn = add_nums,
-    inputs = [gr.Number(label = "First Number"), gr.Number(label = "Second Number")],
+    inputs = [gr.Number(label = "First Number"), gr.Number(label = "Second Number"), gr.Number(label = "Third Number")],
     outputs = gr.Number(label = "Sum")
 )
 
